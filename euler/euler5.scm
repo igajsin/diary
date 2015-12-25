37cc810612ef9a8d979383011149f61764546b8b
“ugoday” <ugoday@gmail.com>
2015-12-25 20:55:24 +0300

    5,9,9a

---
+++ euler/euler5.scm
@@ -0,0 +1,18 @@
+(define-module (euler euler5)
+  #:use-module (euler util)
+  #:use-module (srfi srfi-1))
+
+(define step (* 2 3 5 7 11 13 17 19))
+
+(define init-step (fold (lambda (x acc) (* x acc)) 1 (oseq 2 20)))
+
+(define (check num uborder)
+  (fold (lambda (x acc)
+	  (and acc (is-devisor? num x)))
+	#t
+	(oseq 2 uborder)))
+
+(define (findd k step)
+  (if (check k 20) k
+      (findd (+ k step) step)))
+
---
+++ euler/euler9.scm
@@ -0,0 +1,35 @@
+(define-module (euler euler9)
+  #:use-module (euler util)
+  #:use-module (srfi srfi-1))
+
+(define (is-triplet? triplet)
+  (let ((a (list-ref triplet 0))
+	(b (list-ref triplet 1))
+	(c (list-ref triplet 2)))
+    (= (expt c 2)
+       (+ (expt a 2) (expt b 2)))))
+
+(define (is-correct-triplet? triplet)
+  (let ((a (list-ref triplet 0))
+	(b (list-ref triplet 1))
+	(c (list-ref triplet 2)))
+    (and (is-triplet? triplet)
+	 (= 1000 (+ a b c)))))
+
+(define (mkl2 p? uborder)
+  (let ((triplets '()))
+    (for-each
+     (lambda (c)
+       (for-each (lambda (b) (for-each
+			 (lambda (a)
+			   (let ((triple (list a b c)))
+			     (when (p? triple)
+			       (set! triplets (cons triple triplets)))))
+			 (oseq 1 (1- b))))
+		 (oseq 2 (1- c))))
+     (oseq 3 uborder))
+    triplets))
+
+
+(define triplet '(200 375 425))
+(define result 31875000)
